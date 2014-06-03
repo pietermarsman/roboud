a45a6b63cee9ef40114ddf5632a7b7820c74abc9
Mike Ligthart <mike.ligthart@gmail.com>
2014-06-03 15:57:03 +0200

    start implementing MotorAction #20

--- src/edu/radboud/AI/roboud/action/MotorAction.java
+++ src/edu/radboud/AI/roboud/action/MotorAction.java
@@ -1,15 +1,58 @@
 package edu.radboud.ai.roboud.action;
 
+import com.wowwee.robome.RoboMeCommands;
+import edu.radboud.AI.roboud.action.RobotDirection;
+import edu.radboud.AI.roboud.action.RobotSpeed;
+import edu.radboud.ai.roboud.action.AbstractAction;
 import edu.radboud.ai.roboud.scenario.Scenario;
+import edu.radboud.ai.roboud.RoboudController;
 
 import java.util.Observer;
+
+import static com.wowwee.robome.RoboMeCommands.RobotCommand.*;
 
 /**
  * Created by Pieter Marsman on 27-5-2014.
  */
 public class MotorAction extends AbstractAction {
+
     @Override
-    public void doActions(Scenario scenario, Observer abstractBehaviour) {
-        // TODO
+    public void doActions(Scenario scenario, Observer abstractBehaviour, RoboudController roboudController) {
+        addObserver(abstractBehaviour);
+        //Some how get direction and speed
+        RobotDirection dir = RobotDirection.FORWARD;
+        RobotSpeed speed = RobotSpeed.NORMAL;
+
+        //Not finished
+        switch(dir){
+            case FORWARD:
+                switch(speed){
+                    case FASTEST:
+                        roboudController.sendCommand(kRobot_MoveForwardSpeed1);
+                        break;
+                    case FAST:
+                        roboudController.sendCommand(kRobot_MoveForwardSpeed2);
+                        break;
+                    case NORMAL:
+                        roboudController.sendCommand(kRobot_MoveForwardSpeed3);
+                        break;
+                    case SLOW:
+                        roboudController.sendCommand(kRobot_MoveForwardSpeed4);
+                        break;
+                    case SLOWEST:
+                        roboudController.sendCommand(kRobot_MoveForwardSpeed5);
+                        break;
+                }
+                break;
+            case RIGHT:
+                break;
+            case BACKWARD:
+                break;
+            case LEFT:
+                break;
+        }
+
+        setChanged();
+        notifyObservers();
     }
 }
