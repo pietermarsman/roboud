b7f392cd992827bbeaab39a0bd50c76a3bf435ad
Mike Ligthart <mike.ligthart@gmail.com>
2014-06-03 15:38:33 +0200

    further advances in implementing LED #21

---
+++ src/edu/radboud/AI/roboud/action/LedColor.java
@@ -0,0 +1,7 @@
+package edu.radboud.AI.roboud.action;
+
+/**
+ * Created by Pieter Marsman on 27-5-2014.
+ */
+
+public enum LedColor {BLUE, CYAN, GREEN, ORANGE, RED, WHITE, YELLOW}
--- src/edu/radboud/AI/roboud/behaviour/BehaviourBlock.java
+++ src/edu/radboud/AI/roboud/behaviour/BehaviourBlock.java
@@ -3,12 +3,13 @@ package edu.radboud.ai.roboud.behaviour;
 import edu.radboud.ai.roboud.scenario.Scenario;
 
 import java.util.Observer;
+import edu.radboud.AI.roboud;
 
 /**
  * Created by Pieter Marsman on 2-6-2014.
  */
 public interface BehaviourBlock {
 
-    public void doActions(Scenario scenario, Observer abstractBehaviour);
+    public void doActions(Scenario scenario, Observer abstractBehaviour, RoboudController roboudController);
 
 }
