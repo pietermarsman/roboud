f33af07139b283c68d917499d561f217be2f0d14
Mike Ligthart <mike.ligthart@gmail.com>
2014-06-03 15:21:43 +0200

    advances in implementation of LED action #21

--- src/edu/radboud/AI/roboud/action/LedAction.java
+++ src/edu/radboud/AI/roboud/action/LedAction.java
@@ -9,16 +9,32 @@ import java.util.Observer;
  * Created by Pieter Marsman on 27-5-2014.
  */
 
-public enum class LedColor {};
+public enum LedColor {BLUE, CYAN, GREEN, ORANGE, RED, WHITE, YELLOW};
 
 public class LedAction extends edu.radboud.AI.roboud.action.AbstractAction {
 
     @Override
-    public void doActions(Scenario scenario, Observer abstractBehaviour) {
+    public void doActions(Scenario scenario, Observer abstractBehaviour, LedColor color) {
         addObserver(abstractBehaviour);
         RobotCommand.kRobot_ShowMoodOff;
         RobotCommand.kRobot_HeartBeatOff;
-        RobotCommand.kRobot_RGBHeartBlue;
+        switch(color){
+
+            case BLUE:
+                break;
+            case CYAN:
+                break;
+            case GREEN:
+                break;
+            case ORANGE:
+                break;
+            case RED:
+                break;
+            case WHITE:
+                break;
+            case YELLOW:
+                break;
+        }
         setChanged();
         notifyObservers();
     }
