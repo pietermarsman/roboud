7855704b77f12171d6f560688172b6df99b88b1e
pietermarsman <pietermarsman@gmail.com>
2014-06-09 16:11:48 +0200

    CombineAction implemented, fixes #25

--- src/edu/radboud/AI/roboud/behaviour/TestBehavior.java
+++ src/edu/radboud/AI/roboud/behaviour/TestBehavior.java
@@ -1,7 +1,9 @@
 package edu.radboud.ai.roboud.behaviour;
 
 import edu.radboud.ai.roboud.RoboudController;
-import edu.radboud.ai.roboud.action.ConfirmationAction;
+import edu.radboud.ai.roboud.action.CombineAction;
+import edu.radboud.ai.roboud.action.ListenAction;
+import edu.radboud.ai.roboud.action.ShowTextAction;
 
 import java.util.Observer;
 
@@ -12,7 +14,9 @@ public class TestBehavior extends AbstractBehavior {
 
     public TestBehavior(RoboudController controller, Observer observer) {
         super(controller, observer);
-        ConfirmationAction confirmationAction = new ConfirmationAction(controller, "Are you really, really sure?");
-        blocks.add(confirmationAction);
+        ShowTextAction showTextAction = new ShowTextAction(controller, "Hello");
+        ListenAction listenAction = new ListenAction(controller);
+        CombineAction combineAction = new CombineAction(controller, showTextAction, listenAction);
+        blocks.add(combineAction);
     }
 }
