8b97f7a2775360aeda5946930d16e96988e27ed4
Mike Ligthart <mike.ligthart@gmail.com>
2014-06-20 18:28:18 +0200

    tested to see multi-tasking

--- src/edu/radboud/AI/roboud/behaviour/TestBehavior.java
+++ src/edu/radboud/AI/roboud/behaviour/TestBehavior.java
@@ -1,11 +1,11 @@
 package edu.radboud.ai.roboud.behaviour;
 
-        import edu.radboud.ai.roboud.RoboudController;
-        import edu.radboud.ai.roboud.action.SpeakAction;
-        import edu.radboud.ai.roboud.task.SpeechRepertoire;
-        import edu.radboud.ai.roboud.task.TaskFactory;
+import edu.radboud.ai.roboud.RoboudController;
+import edu.radboud.ai.roboud.action.SpeakAction;
+import edu.radboud.ai.roboud.task.SpeechRepertoire;
+import edu.radboud.ai.roboud.task.TaskFactory;
 
-        import java.util.Observer;
+import java.util.Observer;
 
 /**
  * Created by Pieter Marsman on 4-6-2014.
@@ -17,6 +17,8 @@ public class TestBehavior extends AbstractBehavior {
     public TestBehavior(RoboudController controller, TaskFactory taskFactory, Observer observer) {
         super(controller, taskFactory, observer);
         blocks.add(taskFactory.getDutchFlagLedTask());
+        //blocks.add(new MotorAction(controller, RobotDirection.FORWARD, RobotSpeed.NORMAL));
         blocks.add(new SpeakAction(controller, SpeechRepertoire.textIntroduceMyself));
+        //blocks.add(new MotorAction(controller, RobotDirection.BACKWARD, RobotSpeed.NORMAL));
     }
 }
