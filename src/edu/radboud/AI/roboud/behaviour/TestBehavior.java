624f95abfe00a87de8f1b3e65e140b29230c583d
Mike Ligthart <mike.ligthart@gmail.com>
2014-06-20 18:12:43 +0200

    merging was not complete, is now fixed

--- src/edu/radboud/AI/roboud/behaviour/TestBehavior.java
+++ src/edu/radboud/AI/roboud/behaviour/TestBehavior.java
@@ -1,15 +1,11 @@
 package edu.radboud.ai.roboud.behaviour;
 
-import edu.radboud.ai.roboud.RoboudController;
-import edu.radboud.ai.roboud.action.SpeakAction;
-import edu.radboud.ai.roboud.task.SpeechRepertoire;
-import edu.radboud.ai.roboud.task.TaskFactory;
+        import edu.radboud.ai.roboud.RoboudController;
+        import edu.radboud.ai.roboud.action.SpeakAction;
+        import edu.radboud.ai.roboud.task.SpeechRepertoire;
+        import edu.radboud.ai.roboud.task.TaskFactory;
 
-import java.util.Observer;
-
-<<<<<<<HEAD
-        =======
-        >>>>>>>7b32138d445ff27141faa06946bd706d357a2cd0
+        import java.util.Observer;
 
 /**
  * Created by Pieter Marsman on 4-6-2014.
@@ -20,19 +16,7 @@ public class TestBehavior extends AbstractBehavior {
 
     public TestBehavior(RoboudController controller, TaskFactory taskFactory, Observer observer) {
         super(controller, taskFactory, observer);
-        <<<<<<<HEAD
         blocks.add(taskFactory.getDutchFlagLedTask());
-        =======
-        //SpeakAction speakAction = new SpeakAction(controller, "Hello Yo mama is so fat see is");
-        SpeakAction speakAction = new SpeakAction(controller, SpeechRepertoire.textIntroduceMyself);
-        blocks.add(speakAction);
-
-
-        /*ShowTextAction showTextAction = new ShowTextAction(controller, "Hello");
-        ListenAction listenAction = new ListenAction(controller);
-        CombineAction combineAction = new CombineAction(controller, showTextAction, listenAction);
-        blocks.add(combineAction);
-        */
-        >>>>>>>7 b32138d445ff27141faa06946bd706d357a2cd0
+        blocks.add(new SpeakAction(controller, SpeechRepertoire.textIntroduceMyself));
     }
 }
